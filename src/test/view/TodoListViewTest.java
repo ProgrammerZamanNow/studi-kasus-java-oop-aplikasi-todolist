c8a85ee3832f0ce513342c1015a122f5d31f5e5a
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2021-01-11 12:28:23 +0700

    Test View : Menghapus TodoList

--- src/test/view/TodoListViewTest.java
+++ src/test/view/TodoListViewTest.java
@@ -9,7 +9,7 @@ import view.TodoListView;
 public class TodoListViewTest {
 
   public static void main(String[] args) {
-    testAddTodoList();
+    testRemoveTodoList();
   }
 
   public static void testShowTodoList(){
@@ -34,6 +34,22 @@ public class TodoListViewTest {
     todoListService.showTodoList();
 
     todoListView.addTodoList();
+
+    todoListService.showTodoList();
+  }
+
+  public static void testRemoveTodoList(){
+    TodoListRepository todoListRepository = new TodoListRepositoryImpl();
+    TodoListService todoListService = new TodoListServiceImpl(todoListRepository);
+    TodoListView todoListView = new TodoListView(todoListService);
+
+    todoListService.addTodoList("Belajar Java Dasar");
+    todoListService.addTodoList("Belajar Java OOP");
+    todoListService.addTodoList("Belajar Java Standard Classes");
+
+    todoListService.showTodoList();
+
+    todoListView.removeTodoList();
 
     todoListService.showTodoList();
   }
