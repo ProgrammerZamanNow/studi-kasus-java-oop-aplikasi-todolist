03bf16e46a18c0c2d69f60f6c4923287ceaa946e
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2021-01-11 11:41:34 +0700

    Service & Repository Menambah TodoList

--- src/repository/TodoListRepositoryImpl.java
+++ src/repository/TodoListRepositoryImpl.java
@@ -2,7 +2,7 @@ package repository;
 
 import entity.Todolist;
 
-public class TodoListRepositoryImpl implements TodoListRepository{
+public class TodoListRepositoryImpl implements TodoListRepository {
 
   public Todolist[] data = new Todolist[10];
 
@@ -11,9 +11,41 @@ public class TodoListRepositoryImpl implements TodoListRepository{
     return data;
   }
 
+  public boolean isFull() {
+    var isFull = true;
+    for (int i = 0; i < data.length; i++) {
+      if (data[i] == null) {
+        // model masih ada yang kosong
+        isFull = false;
+        break;
+      }
+    }
+    return isFull;
+  }
+
+  public void resizeIfFull() {
+    // jika penuh, kita resize ukuran array 2x lipat
+    if (isFull()) {
+      var temp = data;
+      data = new Todolist[data.length * 2];
+
+      for (int i = 0; i < temp.length; i++) {
+        data[i] = temp[i];
+      }
+    }
+  }
+
   @Override
   public void add(Todolist todolist) {
+    resizeIfFull();
 
+    // tambahkan ke posisi yang data array nya NULL
+    for (var i = 0; i < data.length; i++) {
+      if (data[i] == null) {
+        data[i] = todolist;
+        break;
+      }
+    }
   }
 
   @Override
--- src/service/TodoListServiceImpl.java
+++ src/service/TodoListServiceImpl.java
@@ -28,7 +28,9 @@ public class TodoListServiceImpl implements TodoListService {
 
   @Override
   public void addTodoList(String todo) {
-
+    Todolist todolist = new Todolist(todo);
+    todoListRepository.add(todolist);
+    System.out.println("SUKSE MENAMBAH TODO : " + todo);
   }
 
   @Override
